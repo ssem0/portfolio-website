54d0937ff83c9db877f86ea525e779b3f9379ab2
ssem0 <ssem@umassd.edu>
2026-06-30 23:27:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,22 @@ document.querySelectorAll('.menu-overlay nav a').forEach(link => {
     });
 });
 
-function viewResume() {
-    // resume link
-    window.open();
+function switchTab(event, tabId) {
+  // hide all tab content panes
+  const contents = document.querySelectorAll('.tab-content');
+  contents.forEach(content => {
+    content.classList.remove('active-content');
+  });
+
+  // remove the active state from all tab buttons
+  const tabs = document.querySelectorAll('.tab-btn');
+  tabs.forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  // show the current targeted tab content pane
+  document.getElementById(tabId).classList.add('active-content');
+
+  // highlight the clicked button as active
+  event.currentTarget.classList.add('active');
 }
